79228aacb4aec486433b1f4fd39ac7e8b95f806e
Adam Kramer <ubuntu@ip-172-31-36-230.us-west-2.compute.internal>
2013-07-09 21:32:59 +0000

    Extra functions

--- web2.js
+++ web2.js
@@ -6,15 +6,6 @@ app.get('/', function(request, response) {
   response.send('Hello World2!');
 });
 
-
-var buffer = fs.readFileSync('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
-
-buf = new Buffer(256);
-len = buf.toString();
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
