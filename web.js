9692ab3d49321ee720fab6f5454e1c20f3e930e0
Adam Kramer <ubuntu@ip-172-31-36-230.us-west-2.compute.internal>
2013-07-09 20:45:29 +0000

    trying to read file

--- web.js
+++ web.js
@@ -2,9 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
+var text21 = fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
 });
+
+app.get('/', function(request, response) {
+  response.send(text21);
+});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
