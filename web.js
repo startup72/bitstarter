d9e002123edd7e55ee53ea40a275746a251c8bfc
Adam Kramer <ubuntu@ip-172-31-36-230.us-west-2.compute.internal>
2013-07-09 21:18:52 +0000

    Back again

--- web.js
+++ web.js
@@ -2,17 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var text21 = fs.readFileSync('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
-
 app.get('/', function(request, response) {
-  response.send(text21);
+  response.send('Hello World2!');
 });
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
